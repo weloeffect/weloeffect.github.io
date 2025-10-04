ba5f400bdc0a18cc198c3d45001680605af705c6
Walid Abdela <walidabdela.pro@gmail.com>
2025-10-04 10:03:24 +0200

    edit with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/", // Add this for GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
